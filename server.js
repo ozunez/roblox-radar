f2951613299ff53b3a653395b2f0da469b3e6bf1
EIEI <123833061+ozunez@users.noreply.github.com>
2026-07-08 04:10:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static("public"));
 
-app.listen(PORT, () => {
+let radarData = {
+    players: []
+};
+
+app.post("/update", (req, res) => {
+    radarData = req.body;
+
+    io.emit("radar", radarData);
+
+    res.sendStatus(200);
+});
+
+io.on("connection", (socket) => {
+    console.log("Viewer Connected");
+
+    socket.emit("radar", radarData);
+});
+
+server.listen(PORT, () => {
     console.log("Radar running on port " + PORT);
 });
