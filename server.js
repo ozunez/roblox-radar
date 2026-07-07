2ea11191800f625490dead164d8ff21187f7bc07
EIEI <123833061+ozunez@users.noreply.github.com>
2026-07-08 04:08:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Roblox Radar is running!");
-});
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log("Radar running on port " + PORT);
 });
